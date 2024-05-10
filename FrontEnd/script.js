7b635b6c45a0f21130abecd4c4c8bc33dfc3b080
David Horès <hores.d@gmail.com>
2024-05-10 11:42:05 +0200

    Add await operator to json function and get first figure from works route with worksGallery function then add it to DOM in script.js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,7 +3,8 @@ let works = window.localStorage.getItem("worksLocal");
 
 // Si aucune donnée en mémoire: téléchargement des données, sinon: écriture des données en JavaScript
 if (works === null) {
-    const swagger = await fetch("http://localhost:5678/api/works/").then(works => swagger.json());
+    const swagger = await fetch("http://localhost:5678/api/works/");
+    works = await swagger.json();
     const worksContent = JSON.stringify(works);
     window.localStorage.setItem("worksLocal", worksContent);
     // Vérification du téléchargement des données
@@ -16,16 +17,26 @@ if (works === null) {
 
 // Fonction de génération de la galerie photos
 function worksGallery (works) {
+    // Récupération du bloc "gallery"
     const gallery = document.querySelector(".gallery");
-    const figure1 = document.createElement("figure");
+    // Récupération du premier élément du tableau works dans une constante
+    const figure0 = works[0];
+    // Création de la balise figure
+    const figure = document.createElement("figure");
+    // Création de la balise image
     const img = document.createElement("img");
+    // Récupération du chemin des images et du titre du work pour les attributs src et alt
+    img.src = figure0.imageUrl;
+    img.alt = figure0.title;
+    // Création de la balise figcaption
     const figcaption = document.createElement("figcaption");
-    img.src = "./assets/images/abajour-tahina.png";
-    img.alt = "Abajour Tahina";
-    figcaption.innerText = "Abajour Tahina";
-    figure1.appendChild(img);
-    figure1.appendChild(figcaption);
-    gallery.appendChild(figure1);
+    // Récupération du titre du work pour la balise figcaption
+    figcaption.innerText = figure0.title;
+    // Ajout de la balise figure au bloc "gallery"
+    gallery.appendChild(figure);
+    // Ajout des balises image et figcaption à la balise figure
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
 }
 
-worksGallery();
+worksGallery(works);
