05d92d72249a5ac6d5fab444221d9baa5f6d3a31
David Horès <hores.d@gmail.com>
2024-05-10 10:25:43 +0200

    Add module type to script.js and remove all figures from gallery div in index.html and add worksGallery function with first figure added to DOM in script.js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -6,10 +6,26 @@ if (works === null) {
     const swagger = await fetch("http://localhost:5678/api/works/").then(works => swagger.json());
     const worksContent = JSON.stringify(works);
     window.localStorage.setItem("worksLocal", worksContent);
-    /* Vérification du téléchargement des données
-    console.log("Données téléchargées"); */
+    // Vérification du téléchargement des données
+    // console.log("Données téléchargées");
 } else {
     works = JSON.parse(works);
-    /* Vérification de la présence des données en mémoire
-    console.log("Données en mémoire"); */
+    // Vérification de la présence des données en mémoire
+    // console.log("Données en mémoire");
 }
+
+// Fonction de génération de la galerie photos
+function worksGallery (works) {
+    const gallery = document.querySelector(".gallery");
+    const figure1 = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    img.src = "./assets/images/abajour-tahina.png";
+    img.alt = "Abajour Tahina";
+    figcaption.innerText = "Abajour Tahina";
+    figure1.appendChild(img);
+    figure1.appendChild(figcaption);
+    gallery.appendChild(figure1);
+}
+
+worksGallery();
