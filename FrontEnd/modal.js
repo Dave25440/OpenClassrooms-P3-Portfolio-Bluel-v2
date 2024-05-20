c1fc66f06474bb615e7f4034a51722438ae67b13
David Horès <hores.d@gmail.com>
2024-05-20 23:45:56 +0200

    Add images from gallery block into modal in modal.js

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -9,11 +9,20 @@ export function modalInit () {
     modalTitle.innerText = "Galerie photo";
     // Création de la balise div
     const modalGallery = document.createElement("div");
+    // Récupération de toutes les balises img du bloc "gallery"
+    const galleryImg = document.querySelector(".gallery").querySelectorAll("img");
     // Ajout de la balise dialog à la section "portfolio"
     document.getElementById("portfolio").appendChild(modal);
     // Ajout du titre "Galerie photo" et de la balise div à la balise dialog
     modal.appendChild(modalTitle);
     modal.appendChild(modalGallery);
+    // Parcours des balises img du bloc "gallery" via une boucle
+    galleryImg.forEach(function (img) {
+        // Création de la balise figure
+        const modalFigure = document.createElement("figure");
+        // Ajout de la balise figure à div et de l'img à figure
+        modalGallery.appendChild(modalFigure).appendChild(img);
+    });
     // Affichage de la modale
     // modal.showModal();
 }
